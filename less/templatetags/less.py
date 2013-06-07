edfb87248f6988ea9df2c4fa1e8fd0a6d8616eef
Richard Antecki <richard@antecki.id.au>
2013-06-07 15:17:02 +1000

    Added new file watching mechanism that caters for cross-app includes.

--- less/templatetags/less.py
+++ less/templatetags/less.py
@@ -3,7 +3,7 @@ from ..cache import get_cache_key, get_hexdigest, get_hashed_mtime
 from ..utils import compile_less
 from ..settings import LESS_EXECUTABLE, LESS_USE_CACHE,\
     LESS_CACHE_TIMEOUT, LESS_ROOT, LESS_OUTPUT_DIR, LESS_DEVMODE,\
-    LESS_DEVMODE_WATCH_DIRS
+    LESS_DEVMODE_WATCH_DIRS, LESS_INCLUDE_ACROSS_APPS
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.core.cache import cache
@@ -12,13 +12,15 @@ import logging
 import subprocess
 import os
 import sys
+import re
 
+from django.contrib.staticfiles import finders
 
 STATIC_ROOT = getattr(settings, "STATIC_ROOT", getattr(settings, "MEDIA_ROOT"))
 
 
 logger = logging.getLogger("less")
-
+from pprint import pformat
 
 register = Library()
 
@@ -91,6 +93,72 @@ def less_paths(path):
 
     return full_path, file_name, output_dir
 
+WATCHED_FILES = {}
+WATCHED_FILES_BY_PARENT = {}
+LESS_IMPORT_RE = re.compile(r"""@import\s+['"](.+?\.less)['"]\s*;""")
+
+def check_file_and_dependencies(path):
+    logger.info("check_file_and_dependencies: "+path)
+    full_path, file_name, output_dir = less_paths(path)
+    mtime = os.path.getmtime(full_path)
+    changed = False
+
+    logger.info("check_file_and_dependencies: 1")
+
+    if path not in WATCHED_FILES:
+        WATCHED_FILES[path] = [None, set()]
+
+    if WATCHED_FILES[path][0] != mtime:
+        changed = True
+        WATCHED_FILES[path][0] = mtime
+
+        logger.info("check_file_and_dependencies: 2")
+
+        # update dependencies
+        WATCHED_FILES_BY_PARENT[path] = set()
+
+        for line in open(full_path):
+            for imported in LESS_IMPORT_RE.findall(line):
+                # test if the include is relative to current file or relative to static root
+                logger.debug("found imported file: "+imported)
+                try_local = True
+                if LESS_INCLUDE_ACROSS_APPS:
+                    # use staticfiles finder to locate file
+                    logger.debug("locating imported file "+imported+" using staticfiles finder")
+                    result = finders.find(imported, all=False)
+                    if result:
+                        imported = os.path.relpath(result, STATIC_ROOT)
+                        try_local = False
+
+                if try_local:
+                    # treat as path relative to current file
+                    logger.debug("locating imported file "+imported+" using relative path")
+                    imported = os.path.relpath(os.path.join(os.path.dirname(full_path), imported), STATIC_ROOT)
+                    logger.debug("resolved import as "+imported)
+
+                if imported not in WATCHED_FILES:
+                    WATCHED_FILES[imported] = [None, set([path])]
+                else:
+                    WATCHED_FILES[imported][1].add(path)
+
+                WATCHED_FILES_BY_PARENT[path].add(imported)
+
+    # check dependencies
+
+    for imported in WATCHED_FILES_BY_PARENT[path]:
+        if check_file_and_dependencies(imported):
+            # a dependency has been modified
+            #logger.debug("** a dependency has been modified")
+            changed = True
+
+    #logger.debug("watched files: "+pformat(WATCHED_FILES))
+
+    if changed:
+        #logger.debug("**** file has been modified, returning true")
+        return True
+
+    #logger.debug("no modifications, returning false")
+    return False
 
 @register.simple_tag
 def less(path):
@@ -112,7 +180,16 @@ def less(path):
         filesystem_encoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
         encoded_full_path = full_path.encode(filesystem_encoding)
 
-    if not os.path.exists(output_path):
+    recompile = False
+
+    if settings.DEBUG:
+        # check for file modifications and recompile file if necessary
+        if check_file_and_dependencies(path):
+            logger.debug("file "+path+" has been modified, recompiling...");
+            recompile = True
+
+    if recompile or not os.path.exists(output_path):
+        logger.debug("compiling file")
         if not compile_less(encoded_full_path, output_path, path):
             return path
 
